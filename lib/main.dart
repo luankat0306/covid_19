871dec4c2e1b1f3945601185fa9206a63971fd6f
TheWarlord <votriluan99@gmail.com>
2020-08-15 23:29:44 +0700

    luan

--- lib/main.dart
+++ lib/main.dart
@@ -28,18 +28,46 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<Country> countryForDisplay = countrys;
+
+  @override
+  void initState() {
+
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: <Widget>[
           AppBar(),
+          Container(
+            padding: EdgeInsets.all(15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(Icons.flag, color: Colors.redAccent,),
+                Text(
+                  "333 new cases ",
+                  style: TextStyle(fontSize:20, color: Colors.redAccent),
+                ),
+                Text(
+                    "in ",
+                  style: TextStyle(fontSize:17, color: Colors.grey),
+                ),
+                Text(
+                  "USA",
+                  style: TextStyle(fontSize:20, color: Colors.blueAccent),
+                ),
+              ],
+            ),
+          ),
           Expanded(
               child: ListView.builder(
-                itemCount: countrys.length,
+                itemCount: countryForDisplay.length,
                 itemBuilder: (context, index) => CountryCard(
                   itemIndex: index,
-                  country: countrys[index],
+                  country: countryForDisplay[index],
                 ),
               )
           ),
@@ -47,8 +75,121 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
-}
+  AppBar () {
+    var size = MediaQuery.of(context).size;
+    return Container(
+      height: size.height * .40,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            height: size.height * .40 - 35,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(40),
+                bottomLeft: Radius.circular(40),
+              ),
+            ),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: FlareActor(
+                    "assets/animations/covid.flr",
+                    alignment: Alignment.center,
+                    fit: BoxFit.contain,
+                    animation: countryForDisplay.isEmpty ? "Buon" : "Hello",
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "All you need is\nstay at home",
+                      textAlign: TextAlign.right,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold),
+                    ),
 
+//Search bar
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 20),
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      width: size.width * .55,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(50),
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(0, 10),
+                            blurRadius: 10,
+                            color: Colors.black26.withOpacity(0.2),
+                          ),
+                        ],
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.search),
+                          hintText: "Search Country",
+                          enabledBorder: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                        ),
+                        onChanged: (text) {
+                          text = text.toLowerCase();
+                          setState(() {
+                            countryForDisplay = countrys.where((contry) {
+                              var contryName = contry.name.toLowerCase();
+                              return contryName.contains(text);
+                            }).toList();
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+
+//Main Counter
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                MainCounter(
+                    size: size,
+                    title: "Total Cases",
+                    numbers: "18,118,726",
+                    color: Colors.amberAccent),
+                MainCounter(
+                  size: size,
+                  title: "Total Deaths",
+                  numbers: "690,514",
+                  color: Colors.redAccent,
+                ),
+                MainCounter(
+                  size: size,
+                  title: "Total Recovered",
+                  numbers: "11,397,014",
+                  color: Colors.green,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+}
+// APP BAR
 class CountryCard extends StatelessWidget {
   final int itemIndex;
   final Country country;
@@ -95,20 +236,21 @@ class CountryCard extends StatelessWidget {
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(5),
                   child: Image.asset(
-                      country.flag,
-                      fit: BoxFit.cover,
+                    country.flag,
+                    fit: BoxFit.cover,
                   ),
                 ),
               ),
               Text(
-                  country.name,
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
-                    shadows: [
-                      Shadow(offset: Offset(0, 10), blurRadius: 30, color: Colors.grey)
-                    ],
-                  ),
+                country.name,
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.black54,
+                  fontWeight: FontWeight.bold,
+                  shadows: [
+                    Shadow(offset: Offset(0, 10), blurRadius: 30, color: Colors.grey)
+                  ],
+                ),
               ),
             ],
           ),
@@ -137,118 +279,6 @@ class CountryCard extends StatelessWidget {
               ],
             ),
           )
-        ],
-      ),
-    );
-  }
-}
-// APP BAR
-class AppBar extends StatefulWidget {
-  @override
-  _AppBarState createState() => _AppBarState();
-}
-
-class _AppBarState extends State<AppBar> {
-  @override
-  Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size;
-    return Container(
-      height: size.height * .40,
-      child: Stack(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            height: size.height * .40 - 35,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.only(
-                bottomRight: Radius.circular(40),
-                bottomLeft: Radius.circular(40),
-              ),
-            ),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: FlareActor(
-                    "assets/animations/covid.flr",
-                    alignment: Alignment.center,
-                    fit: BoxFit.contain,
-                    animation: "Hello",
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      "All you need is\nstay at home",
-                      textAlign: TextAlign.right,
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold),
-                    ),
-
-//Search bar
-                    Container(
-                      margin: EdgeInsets.symmetric(vertical: 20),
-                      padding: EdgeInsets.symmetric(horizontal: 20),
-                      width: size.width * .55,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(50),
-                        boxShadow: [
-                          BoxShadow(
-                            offset: Offset(0, 10),
-                            blurRadius: 10,
-                            color: Colors.black26.withOpacity(0.2),
-                          ),
-                        ],
-                      ),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          icon: Icon(Icons.search),
-                          hintText: "Search Country",
-                          enabledBorder: InputBorder.none,
-                          focusedBorder: InputBorder.none,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-
-//Main Counter
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                MainCounter(
-                    size: size,
-                    title: "Cases",
-                    numbers: "18,118,726",
-                    color: Colors.amberAccent),
-                MainCounter(
-                  size: size,
-                  title: "Deaths",
-                  numbers: "690,514",
-                  color: Colors.redAccent,
-                ),
-                MainCounter(
-                  size: size,
-                  title: "Recovered",
-                  numbers: "11,397,014",
-                  color: Colors.green,
-                ),
-              ],
-            ),
-          ),
         ],
       ),
     );
@@ -301,7 +331,7 @@ class MainCounter extends StatelessWidget {
           Text(
             title,
             style: TextStyle(
-              color: color.withOpacity(0.8),
+              fontSize:12, color: color.withOpacity(0.8),
             ),
           ),
         ],
--- lib/models/country.dart
+++ lib/models/country.dart
@@ -1,74 +1,44 @@
 class Country{
-  final int id, cases, recovered, deaths;
-  final String name, flag;
+  final String name, flag, cases, recovered, deaths;
 
-  Country({this.id, this.cases, this.recovered, this.deaths, this.name, this.flag});
+  Country({this.cases, this.recovered, this.deaths, this.name, this.flag});
 
 }
 
 List<Country> countrys = [
   Country(
-    id: 1,
-    cases: 1203588,
-    recovered: 255555,
-    deaths: 3555,
-    flag: 'assets/flags/us.png',
-    name: 'USA'
-  ),
-  Country(
-      id: 2,
-      cases: 753578,
-      recovered: 353595,
-      deaths: 155,
-      flag: 'assets/flags/in.png',
-      name: 'Indian'
-  ),
-  Country(
-      id: 1,
-      cases: 1203588,
-      recovered: 255555,
-      deaths: 3555,
+      cases: '5,476,266',
+      recovered: '2,875,147',
+      deaths: '171,535',
       flag: 'assets/flags/us.png',
       name: 'USA'
   ),
   Country(
-      id: 2,
-      cases: 753578,
-      recovered: 353595,
-      deaths: 155,
+      cases: '3,278,895',
+      recovered: '2,384,302',
+      deaths: '106,571',
+      flag: 'assets/flags/bra.png',
+      name: 'Brazil'
+  ),
+  Country(
+      cases: '2,553,578',
+      recovered: '353,595',
+      deaths: '155',
       flag: 'assets/flags/in.png',
       name: 'Indian'
   ),
   Country(
-      id: 1,
-      cases: 1203588,
-      recovered: 255555,
-      deaths: 3555,
-      flag: 'assets/flags/us.png',
-      name: 'USA'
+      cases: '2,553,578',
+      recovered: '353,595',
+      deaths: '155',
+      flag: 'assets/flags/rus.png',
+      name: 'Russia',
   ),
   Country(
-      id: 2,
-      cases: 753578,
-      recovered: 353595,
-      deaths: 155,
-      flag: 'assets/flags/in.png',
-      name: 'Indian'
-  ),
-  Country(
-      id: 1,
-      cases: 1203588,
-      recovered: 255555,
-      deaths: 3555,
-      flag: 'assets/flags/us.png',
-      name: 'USA'
-  ),
-  Country(
-      id: 2,
-      cases: 753578,
-      recovered: 353595,
-      deaths: 155,
-      flag: 'assets/flags/in.png',
-      name: 'Indian'
-  ),
+      cases: '950',
+      recovered: '437	',
+      deaths: '23',
+      flag: 'assets/flags/vn.png',
+      name: 'Vietnam'
+  )
 ];
